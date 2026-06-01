712b5f5b5e486876295ac32030cc2d0b2c56a6a1
Muhammad Fardeen <m.fdeen02@gmail.com>
2026-05-31 22:35:26 -0500

    dont retain cooldown indefinitely

--- src/main/java/org/maboroshi/vessel/handler/CooldownHandler.java
+++ src/main/java/org/maboroshi/vessel/handler/CooldownHandler.java
@@ -1,14 +1,16 @@
 package org.maboroshi.vessel.handler;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 public class CooldownHandler {
-    private final Map<UUID, Long> interactionCooldowns = new HashMap<>();
+    private final Cache<UUID, Long> interactionCooldowns =
+            CacheBuilder.newBuilder().expireAfterWrite(5, TimeUnit.MINUTES).build();
 
     public boolean isOnCooldown(UUID playerId, long cooldownMs) {
-        Long last = interactionCooldowns.get(playerId);
+        Long last = interactionCooldowns.getIfPresent(playerId);
         return last != null && System.currentTimeMillis() - last < cooldownMs;
     }
 
